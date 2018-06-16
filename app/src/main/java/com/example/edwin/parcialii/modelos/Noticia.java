6110a0356bf9fb3bb2b5b7bb92afc99ae6073fc2
Edwin Morales <edwin.morales9b@gmail.com>
2018-06-15 21:35:44 -0600

    Dao y Entidad de Room Creadas

--- app/src/main/java/com/example/edwin/parcialii/modelos/Noticia.java
+++ app/src/main/java/com/example/edwin/parcialii/modelos/Noticia.java
@@ -1,110 +1,135 @@
 package com.example.edwin.parcialii.modelos;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
-
+/*import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;*/
 import java.io.Serializable;
-import java.util.Date;
 
-public class Noticia implements Comparable<Noticia>, Serializable{
-    private String ID;
-    private String noticia;
-    private String titulo;
-    private int foto;
-    private String juego;
-    private String subtitulo;
-    private Date fechaInit;
-    private String autor;
-    private String imgPortada;
-    private String fechaInicial;
+public class Noticia implements Serializable, Parcelable, Comparable<Noticia>{
+    private String _id;
+
+    private String title;
+
+    private String coverImage;
+
+    private String created_date;
+
+    private String description;
+
+    private String body;
+
+    private String game;
+
+
+    public Noticia(@NonNull String id, String title, String coverImage, String created_date, String description, String body, String game) {
+        _id = id;
+        this.title = title;
+        this.coverImage = coverImage;
+        this.created_date = created_date;
+        this.description = description;
+        this.body = body;
+        this.game = game;
+    }
 
     public Noticia() {
     }
 
-    public Noticia(String ID, String noticia, String titulo, int foto) {
-        this.ID = ID;
-        this.noticia = noticia;
-        this.titulo = titulo;
-        this.foto = foto;
+    protected Noticia(Parcel in) {
+        _id = in.readString();
+        title = in.readString();
+        coverImage = in.readString();
+        created_date = in.readString();
+        description = in.readString();
+        body = in.readString();
+        game = in.readString();
     }
 
-    public String getFechaInicial() {
-        return fechaInicial;
+    public static final Creator<Noticia> CREATOR = new Creator<Noticia>() {
+        @Override
+        public Noticia createFromParcel(Parcel in) {
+            return new Noticia(in);
+        }
+
+        @Override
+        public Noticia[] newArray(int size) {
+            return new Noticia[size];
+        }
+    };
+
+    @NonNull
+    public String get_id() {
+        return _id;
     }
 
-    public void setFechaInicial(String fechaInicial) {
-        this.fechaInicial = fechaInicial;
+    public void set_id(@NonNull String _id) {
+        this._id = _id;
     }
 
-    public String getImgPortada() {
-        return imgPortada;
+    public String getTitle() {
+        return title;
     }
 
-    public void setImgPortada(String imgPortada) {
-        this.imgPortada = imgPortada;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public String getID() {
-        return ID;
+    public String getCoverImage() {
+        return coverImage;
     }
 
-    public void setID(String ID) {
-        this.ID = ID;
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
     }
 
-    public String getNoticia() {
-        return noticia;
+    public String getCreated_date() {
+        return created_date;
     }
 
-    public void setNoticia(String noticia) {
-        this.noticia = noticia;
+    public void setCreated_date(String created_date) {
+        this.created_date = created_date;
     }
 
-    public String getTitulo() {
-        return titulo;
+    public String getDescription() {
+        return description;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public int getFoto() {
-        return foto;
+    public String getBody() {
+        return body;
     }
 
-    public void setFoto(int foto) {
-        this.foto = foto;
+    public void setBody(String body) {
+        this.body = body;
     }
 
-    public String getJuego() {
-        return juego;
+    public String getGame() {
+        return game;
     }
 
-    public void setJuego(String juego) {
-        this.juego = juego;
+    public void setGame(String game) {
+        this.game = game;
     }
 
-    public String getSubtitulo() {
-        return subtitulo;
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
-    public void setSubtitulo(String subtitulo) {
-        this.subtitulo = subtitulo;
-    }
-
-    public Date getFechaInit() {
-        return fechaInit;
-    }
-
-    public void setFechaInit(Date fechaInit) {
-        this.fechaInit = fechaInit;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(_id);
+        dest.writeString(title);
+        dest.writeString(coverImage);
+        dest.writeString(created_date);
+        dest.writeString(description);
+        dest.writeString(body);
+        dest.writeString(game);
     }
 
     @Override
